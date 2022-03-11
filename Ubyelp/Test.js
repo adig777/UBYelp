57837da144cce16c07aa5e0de7f6525f93af256f
Kevin T Truong <38699598+KevinTTruong@users.noreply.github.com>
2022-03-10 19:06:42 -0800

    Added Test criteria

--- Ubyelp/Test.js
+++ Ubyelp/Test.js
@@ -1,1 +1,18 @@
+/* Test criteria
+ * -Connect to SQL
+ * -Successful Yelp API query
+ * -Successful front-end development (print Hello World or whatever)
+ */ 
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "UBYelp",
+    pass: "cs160UBY3!p",
+    database: "ubyelp",
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected!");
+});
