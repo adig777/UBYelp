4b0919320439665562018831cde85b34ddc86ce5
Aihong Yin <79123600+aihongyin@users.noreply.github.com>
2022-04-17 16:11:48 -0700

    Add files via upload

---
+++ src (search page:result)/components/Business/Business.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Business.css';
+
+class Business extends React.Component {
+    // Renders the individual businesses returned in the search results
+    render() {
+        return (
+            <div className="Business">
+                <div className="image-container">
+                    <img src={this.props.business.imageSrc} alt=''/>
+                </div>
+                <h2>{this.props.business.name}</h2>
+                <div className="Business-information">
+                    <div className="Business-address">
+                        <p>{this.props.business.address}</p>
+                        <p>{this.props.business.city}</p>
+                        <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
+                        <p>{this.props.business.price}</p>   
+
+                    </div>
+                    <div className="Business-reviews">
+                        <h3>{this.props.business.category}</h3>
+                        <h3 className="rating">{`${this.props.business.rating} stars`}</h3>
+                        <p>{`${this.props.business.reviewCount} reviews`}</p>     
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Business;
