51d50411cdfe4899fa9c3410943a960c51985831
adig777 <59784934+adig777@users.noreply.github.com>
2022-04-30 14:44:10 -0700

    Update Routes.js

--- Ubyelp/src/Routes.js
+++ Ubyelp/src/Routes.js
@@ -54,120 +54,99 @@ app.post('/addlistitem', async (req, res) => {
 
 app.post('/deletelistitem', async (req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let List = new ListBackend(input.account_id);
+    let List = new ListBackend(input.id);
     res.end(await List.removeListItem(input.list_id, input.list_item_id));
 });
 
 app.post('/deletelist', async (req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let List = new ListBackend(input.account_id);
+    let List = new ListBackend(input.id);
     res.end(await List.removeList(input.list_id));
 });
 
 app.post('/editlistname', async (req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let List = new ListBackend(input.account_id);
+    let List = new ListBackend(input.id);
     res.end(await List.editListName(input.list_id, input.newTitle));
 });
 
 app.post('/editlistdesc', async (req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let List = new ListBackend(input.account_id);
+    let List = new ListBackend(input.id);
     res.end(await List.editListDescription(input.list_id, input.newDesc));
 });
 
 app.post('/edititemdesc', async (req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let List = new ListBackend(input.account_id);
+    let List = new ListBackend(input.id);
     res.end(await List.editItemDescription(input.list_item_id, input.newDesc));
 });
 
 app.post('/edititemrating', async (req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let List = new ListBackend(input.account_id);
+    let List = new ListBackend(input.id);
     res.end(await List.editItemRating(input.list_item_id, input.rating));
 });
 
 app.post('/edititemname', async (req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let List = new ListBackend(input.account_id);
+    let List = new ListBackend(input.id);
     res.end(await List.editItemName(input.list_item_id, input.name));
 });
 
 app.post('/setdistance', async(req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let Settings = new SettingsBackend(input.account_id);
+    let Settings = new SettingsBackend(input.id);
     res.end(await Settings.setFilterDistance(input.newFilter))
 });
 
 app.post('/setrating', async(req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let Settings = new SettingsBackend(input.account_id);
+    let Settings = new SettingsBackend(input.id);
     res.end(await Settings.setFilterRating(input.newFilter))
 });
 
 app.post('/setpricerange', async(req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let Settings = new SettingsBackend(input.account_id);
-    res.end(await Settings.setFilterPrice(input.newPrices))
+    let Settings = new SettingsBackend(input.id);
+    res.end(await Settings.setFilterPriceRange(input.newMin, input.newMax))
 });
 
 app.post('/setopen', async(req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let Settings = new SettingsBackend(input.account_id);
+    let Settings = new SettingsBackend(input.id);
     res.end(await Settings.setFilterOpen(input.newFilter))
 });
 
 app.post('/setinlist', async(req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let Settings = new SettingsBackend(input.account_id);
+    let Settings = new SettingsBackend(input.id);
     res.end(await Settings.setFilterInList(input.listName))
 });
 
 app.post('/setnotlist', async(req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let Settings = new SettingsBackend(input.account_id);
+    let Settings = new SettingsBackend(input.id);
     res.end(await Settings.setFilterNotInList(input.listName))
 });
 
 app.post('/getlists', async(req, res) => {
     let input = JSON.parse(Object.keys(req.body)[0]);
-    let List = new ListBackend(input.account_id);
-    await List.getLists((list)=>{
+    let List = new ListBackend(input.id);
+    await List.getLists((list) => {
     res.end(list);
     });
 });
+
+// app.post('/listnamesandid', (req, res) => {
+//     let input = JSON.parse(Object.keys(req.body)[0]);
+//     let Search = new SearchBackend(input.id);
+//     Search.getListNamesAndIds((results) => {
+//         res.end(results);
+//     });
+// });
 
 
 app.listen(3001, () => {
     console.log('Route.js running');
 });
-
-/* Fetch calls saved here until Front-ends are done
-
-//Get Default Filters
-fetch('http://localhost:3001/defaultfilters', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: JSON.stringify({
-                'id': id
-            })
-        }).then((response) => response.json()
-        ).then((filters) => {
-            console.log(filters);
-            //this.filters = filters
-        });
-
-//Get list names
-fetch('http://localhost:3001/listnames', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: JSON.stringify({
-                'id': id
-            })
-        }).then((response) => response.json()
-        ).then((names) => {
-            //Set in_list and not_list's dropdown menu values to 'names'
-            //Format: {'list_name': 'list_id', ...}
-        });
-*/
